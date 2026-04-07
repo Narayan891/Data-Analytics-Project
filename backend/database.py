7471a746ea54e3670c13ec1d35a376fbdfe15062
Narayan <narayanvamsi891@gmail.com>
2026-04-07 08:07:02 +0530

    added requirements.txt

--- backend/database.py
+++ backend/database.py
@@ -1,4 +1,7 @@
 from sqlalchemy import create_engine
+import os
+
+DATABASE_URL = os.getenv("postgresql://zoo_db_4zo0_user:mPBvdLFNoUYYEBKUG9KjEfId1zjOlQZH@dpg-d7a6ed7pm1nc73bus3kg-a.oregon-postgres.render.com/zoo_db_4zo0")
 
 DATABASE_URL = "postgresql://postgres:1234@localhost:5432/zoo_db"
 
